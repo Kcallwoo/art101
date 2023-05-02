a0cea16ef431601a80b0c57dfc4b6c959e44a064
Kcallwoo <kcallwoo@ucsc.edu>
2023-05-01 20:03:53 -0700

    lab6

--- lab6/js/lab.js
+++ lab6/js/lab.js
@@ -1,10 +1,27 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+// index.js - Array and Variables: Purpose is to create an array that contains modes of transportation and an object that contains information about a car. 
+// Author: Kamryn Callwood
+// Date: 2 May 2023
 
-// Constants
+// Define Variables
+var myTransport = ["walking", "car", "bus", "bike"];
 
-// Functions
+// Create an object for my main ride
+
+// Functionsvar 
+var myMainRide = {
+    make: "Lamborghini",
+    model: "Urus",
+    color: "Red",
+    year: "2017",
+    age: function() {
+        return 2023 - this.year;
+    }
+  }
+
+// Output
+document.writeln("Kinds of transportation I use: ", myTransport, "</br>");
+document.writeln("My Main Ride: <pre>", 
+    JSON.stringify(myMainRide, null, '\t'), "</pre>");
 
 // this is an example function and this comment tells what it doees and what parameters are passed to it.
 function myFunction(param1, param2) {
