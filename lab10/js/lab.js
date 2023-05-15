f0bab0f7c9953625f49bab84a2fbd746c7aeefb6
Kcallwoo <kcallwoo@ucsc.edu>
2023-05-14 23:14:28 -0700

    lab10

---
+++ lab10/js/lab.js
@@ -0,0 +1,92 @@
+// index.js - JavaScript Events and Forms
+// Author: Kamryn Callwood & Kelsey Melott
+// Date: 16 May 2023
+
+function sortUserName(name) {
+  // Retrieve name
+  var userName = name
+  console.log("userName = ", userName);
+  // split the string into an array
+  var nameArray = userName.split('');
+  // sort the array in alphabetical order, case-insensitive
+  var nameArraySort = nameArray.sort(function(a, b) {
+    var nameA = a.toLowerCase();
+    var nameB = b.toLowerCase();
+    if (nameA < nameB) {
+      return -1;
+    }
+    if (nameA > nameB) {
+      return 1;
+    }
+    return 0;
+  });
+
+  console.log("nameArraySort =", nameArraySort);
+  // change the array back into a string
+  var nameSorted = nameArraySort.join('');
+  console.log("nameSorted =", nameSorted);
+  return nameSorted;
+}
+
+function anagram(str) {
+  // split the string into an array
+  var strArray = str.split('');
+  // shuffle the array
+  for (var i = strArray.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = strArray[i];
+    strArray[i] = strArray[j];
+    strArray[j] = temp;
+  }
+  // join the array back into a string
+  var strShuffled = strArray.join('');
+  console.log("strShuffled =", strShuffled);
+  return strShuffled;
+}
+
+var button = document.getElementById('my-button');
+
+button.addEventListener('click', function() {
+  var inputValue = document.getElementById('user-name').value;
+  const outputDiv = document.getElementById('output');
+  //console.log("You input:", inputValue);
+
+  
+  var sorted = sortUserName(inputValue);
+  var shuffled = anagram(inputValue);
+  outputDiv.innerHTML = "Sorted: " + sorted + "<br>Shuffled: " + shuffled;
+  outputDiv.innerHTML = sorted;
+});
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
+
+
--- lab9/js/lab.js
+++ lab9/js/lab.js
@@ -5,29 +5,32 @@
 
 var outputE1 = document.getElementById("output");
 
-var new1E1 = document.createElement("h2");
+var new1E1 = document.createElement("h1");
+new1E1.innerHTML = "ART-101";
 
+outputE1.insertBefore(new1E1, outputE1.firstChild);
 
-var new2E1 = document.createElement("p");
+var new2E1 = document.createElement("h2");
+var new3E1 = document.createElement("p");
 
-new1E1.innerHTML = "Moral of the story:";
-new2E1.innerHTML = "This lab was hard";
+new2E1.innerHTML = "Moral of the story:";
+new3E1.innerHTML = "This lab was hard";
 
-outputE1.appendChild(new1E1);
 outputE1.appendChild(new2E1);
+outputE1.appendChild(new3E1);
 
-new1E1.style.color = "green";
+new1E1.style.color = "blue";
 new1E1.style.fontWeight = "bold";
-new1E1.style.marginLeft = "15px";
 
-new2E1.style.marginLeft = "15px";
-new2E1.style.textDecoration = "underline";
+new2E1.style.color = "blue";
 new2E1.style.fontWeight = "bold";
+new2E1.style.marginLeft = "15px";
 
+new3E1.style.marginLeft = "15px";
+new3E1.style.textDecoration = "underline";
+new3E1.style.fontWeight = "bold";
 
 var body1 = document.getElementsByClassName("minor-section");
 for (var i = 0; i < body1.length; i++) {
   body1[i].style.color = "red";
 }
-
-
